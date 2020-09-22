265c713c9230e91c934117e694f1a592aa3512f0
TELIDAS <71645703+TELIDAS@users.noreply.github.com>
2020-09-23 01:47:32 +0600

    Delete HOMEWORK_1(15).py

--- HOMEWORK_1(15).py
+++
@@ -1,8 +0,0 @@
-temperature = int(input(""))
-summer = input("")
-if temperature in range(60, 101) and summer == "True":
-    print("True")
-elif temperature in range(60, 91) and summer == "False":
-    print("True")
-else:
-    print("False")
