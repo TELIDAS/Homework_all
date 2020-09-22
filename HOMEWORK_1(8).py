302bae050533deba4a0ee4de82064160a16281eb
TELIDAS <71645703+TELIDAS@users.noreply.github.com>
2020-09-23 01:49:37 +0600

    Delete HOMEWORK_1(8).py

--- HOMEWORK_1(8).py
+++
@@ -1,9 +0,0 @@
-z = int(input(""))
-x = int(input(""))
-negative = input("")
-if (z < 0) and (x < 0) and negative == "True":
-    print("True")
-if z > 0 and x < 0 or z < 0 and x > 0 and negative == "False":
-    print("True")
-else:
-    print("False")
