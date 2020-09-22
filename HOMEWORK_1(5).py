3c4f66b1714a8435b9d99f0c0064181e8f907640
TELIDAS <71645703+TELIDAS@users.noreply.github.com>
2020-09-23 01:48:58 +0600

    Delete HOMEWORK_1(5).py

--- HOMEWORK_1(5).py
+++
@@ -1,8 +0,0 @@
-parrot_trouble = int(input(""))
-talking = input("")
-if (parrot_trouble <= 7) and talking == "yes":
-    print("kill that parrot")
-elif (parrot_trouble >= 20) and talking == "yes":
-    print("kill that parrot")
-else:
-    print("parrot can live")
