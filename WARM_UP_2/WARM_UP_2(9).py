a3bd950fb95e3fbef05f24c7da05f25a0703cc88
TELIDAS <71645703+TELIDAS@users.noreply.github.com>
2020-09-23 23:07:33 +0600

    Delete WARM_UP_2(9).py

--- WARM_UP_2/WARM_UP_2(9).py
+++
@@ -1,9 +0,0 @@
-# string match
-a = input(": ")
-b = input(":")
-min_len = min(len(a), len(b))
-count = 0
-for w in range(min_len -1):
-    if a[w:w+2] == b[w:w+2]:
-        count += 1
-print(count)
